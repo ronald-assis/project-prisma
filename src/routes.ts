d043873f0818e83dbdb26f393ad71509d961580f
Ronald Assis <ronaldassis71@gmail.com>
2022-04-23 18:09:06 -0300

    fix: create category controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { CreateProductController } from "./controllers/CreateProductsController";
+import { CreateCategoryController } from "./controllers/CreateCategoryController";
 
 const router = Router();
 
 const createProduct = new CreateProductController();
+const createCategory = new CreateCategoryController()
 
 router.post('/product', createProduct.handle)
+router.post('/category', createCategory.handle)
+
 
 export { router };
